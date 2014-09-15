1cefe839286dbb14fe5fa2c97eb12ff9b0d69b7d
Ben Griffiths <benjamin.griffiths@nokia.com>
2014-09-15 17:20:04 +0100

    Add ambient lighting and some movement

--- desktop/src-common/music_craft/core.clj
+++ desktop/src-common/music_craft/core.clj
@@ -4,18 +4,33 @@
              [ui :refer :all]
              [g3d :refer :all]
              [math :refer :all]
-             [repl :refer :all]]))
+             [repl :refer :all]
+             [utils :as u]])
+  (:import [com.badlogic.gdx.graphics.g3d.environment DirectionalLight]
+           [com.badlogic.gdx.input RemoteInput]))
+
+(declare music-craft main-screen)
+
+(def remote-input (RemoteInput.))
+(doto remote-input (.setCursorCatched true))
+(doto remote-input (.setCursorPosition 0 0))
+
+(defn directional-light
+  "Returns a new directional light, doesn't seem to exist in play-clj"
+  []
+  (doto (DirectionalLight.)
+    (.set 0.8 0.8 0.8 -1 -0.8 -0.2)))
 
 (defscreen main-screen
   :on-show
   (fn [screen entities]
     (update! screen
              :renderer (model-batch)
-             :attributes (let [attr-type (attribute-type :color :ambient-light)
-                               attr (attribute :color attr-type 0.4 0.4 0.4 1)]
-                           (environment :set attr))
+             :attributes (let [ambient-attr-type (attribute-type :color :ambient-light)
+                               ambient-attr (attribute :color ambient-attr-type 0.4 0.4 0.4 1)]
+                           (environment :set ambient-attr :add (directional-light)))
              :camera (doto (perspective 67 (game :width) (game :height))
-                       (position! 5 5 5)
+                       (position! 0 0 20)
                        (direction! 0 0 0)
                        (near! 0.1)
                        (far! 300)))
@@ -27,6 +42,31 @@
           model
           (assoc :x 0 :y 0 :z 0))))
 
+  :on-key-down
+  (fn [{:keys [key] :as screen} entities]
+    (condp = key
+       (key-code :w)
+       (doto screen
+         (perspective! :translate 0 0 -1)
+         (perspective! :update))
+
+       (key-code :s)
+       (doto screen
+         (perspective! :translate 0 0 1)
+         (perspective! :update))
+
+       (key-code "r")
+       (on-gl (set-screen! music-craft main-screen))
+
+       nil)
+    entities)
+
+  :on-mouse-moved
+  (fn [screen entities]
+    (prn (u/get-obj screen :input-listeners))
+    (prn (input->screen screen (input! :get-x) (input! :get-y)))
+    entities)
+
   :on-render
   (fn [screen entities]
     (clear! 1 1 1 1)
@@ -36,5 +76,3 @@
   :on-create
   (fn [this]
     (set-screen! this main-screen)))
-
-(on-gl (set-screen! music-craft main-screen))
