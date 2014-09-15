52c7426a2b450793a979fc5502911d552f4e840e
Ben Griffiths <benjamin.griffiths@nokia.com>
2014-09-15 11:49:01 +0100

    Initial commit of music-craft from play-clj lein template

---
+++ desktop/project.clj
@@ -0,0 +1,20 @@
+(defproject music-craft "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  
+  :dependencies [[com.badlogicgames.gdx/gdx "1.3.1"]
+                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.3.1"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.3.1"]
+                 [com.badlogicgames.gdx/gdx-box2d-platform "1.3.1"
+                  :classifier "natives-desktop"]
+                 [com.badlogicgames.gdx/gdx-bullet "1.3.1"]
+                 [com.badlogicgames.gdx/gdx-bullet-platform "1.3.1"
+                  :classifier "natives-desktop"]
+                 [com.badlogicgames.gdx/gdx-platform "1.3.1"
+                  :classifier "natives-desktop"]
+                 [org.clojure/clojure "1.6.0"]
+                 [play-clj "0.3.11"]]
+  
+  :source-paths ["src" "src-common"]
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+  :aot [music-craft.core.desktop-launcher]
+  :main music-craft.core.desktop-launcher)
---
+++ desktop/src/music_craft/core/desktop_launcher.clj
@@ -0,0 +1,10 @@
+(ns music-craft.core.desktop-launcher
+  (:require [music-craft.core :refer :all])
+  (:import [com.badlogic.gdx.backends.lwjgl LwjglApplication]
+           [org.lwjgl.input Keyboard])
+  (:gen-class))
+
+(defn -main
+  []
+  (LwjglApplication. music-craft "music-craft" 800 600)
+  (Keyboard/enableRepeatEvents true))
